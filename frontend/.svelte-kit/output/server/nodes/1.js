154c9a8383c62ee1a6332e57f5a750cc69d0080c
Max Richter <jim-x@web.de>
2024-03-06 14:03:25 +0100

    fix: cleanup frontend .svelte-kit

--- frontend/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CSd4wjwx.js","_app/immutable/chunks/scheduler.DQZw9iDR.js","_app/immutable/chunks/index.D-4GBzMI.js","_app/immutable/chunks/entry.DSCOgCkx.js","_app/immutable/chunks/index.CtlmB4aM.js"];
-export const stylesheets = [];
-export const fonts = [];
