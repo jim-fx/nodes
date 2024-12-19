74b7cc42329c4149ad45723d274c20bc9c50ae1c
Max Richter <jim-x@web.de>
2024-12-19 15:09:17 +0100

    WIP

--- store/src/main.ts
+++ store/src/main.ts
@@ -1,18 +1,22 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
 import { router } from "./routes/router.ts";
 import { createUser } from "./routes/user/user.service.ts";
 import { swaggerUI } from "@hono/swagger-ui";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 
-await createUser("max");
+async function init() {
+  const openapi = await router.request("/openapi.json");
+  const json = await openapi.text();
+  Deno.writeTextFile("openapi.json", json);
 
-const app = new OpenAPIHono();
-app.use("/v1/*", cors());
-app.use(logger());
-app.route("v1", router);
+  await createUser("max");
+}
+await init();
 
-app.doc("/openapi.json", {
+router.use(logger());
+router.use(cors());
+
+router.doc("/openapi.json", {
   openapi: "3.0.0",
   info: {
     version: "1.0.0",
@@ -20,6 +24,6 @@ app.doc("/openapi.json", {
   },
 });
 
-app.get("/ui", swaggerUI({ url: "/openapi.json" }));
+router.get("/ui", swaggerUI({ url: "/openapi.json" }));
 
-Deno.serve(app.fetch);
+Deno.serve(router.fetch);
