83cb2bd950466bae7492e8195dcdc350d4183ad9
Max Richter <max.richter@dynabase.de>
2026-01-19 14:04:00 +0100

    feat: move analytics script to env

---
+++ app/src/lib/config.ts
@@ -0,0 +1,2 @@
+import { PUBLIC_ANALYTIC_SCRIPT } from "$env/static/public";
+export const ANALYTIC_SCRIPT = PUBLIC_ANALYTIC_SCRIPT;
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,7 +2,14 @@
   import "@nodarium/ui/app.css";
   import "../app.css";
   import type { Snippet } from "svelte";
+  import * as config from "$lib/config";
   const { children } = $props<{ children?: Snippet }>();
 </script>
 
 {@render children?.()}
+
+<svelte:head>
+  {#if config.ANALYTIC_SCRIPT}
+    {@html config.ANALYTIC_SCRIPT}
+  {/if}
+</svelte:head>
